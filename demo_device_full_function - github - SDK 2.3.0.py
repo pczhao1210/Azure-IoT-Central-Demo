032d4af9bd196f3db06722e00f17b4698a1b8ae4
Pengcheng Zhao <50711491+pczhao1210@users.noreply.github.com>
2021-12-20 11:15:30 +0800

    Update demo_device_full_function - github - SDK 2.3.0.py
    update method and remove connection string connection

--- demo_device_full_function - github - SDK 2.3.0.py
+++ demo_device_full_function - github - SDK 2.3.0.py
@@ -7,14 +7,15 @@ import random
 import uuid
 from functions import derive_device_key
 from six.moves import input
-from azure.iot.device import MethodResponse, Message
 from azure.iot.device.aio import IoTHubDeviceClient, ProvisioningDeviceClient
+from azure.iot.device import MethodResponse, Message
 
 fw_info = 1.0
 telemetry_interval = 10
 send_data = True
+overload_value = 2.45
 
-location = {"Geo_Info":{"value":{"lon": 116.3, "lat": 39.9}}} #Beijing, China
+location = {"Geo_Info":{"value":{"lon": 116.3, "lat": 39.9}}}
 device_property = {
     'Manufacturer': 'Microsoft',
     'Model': 'Azure_Demo_Device',
@@ -28,16 +29,19 @@ device_property = {
     'Device_Country': 'P.R. China'
 }
 
+
+# Using this for Connecting through Device Provisioning Service (DPS) - "Group Enrollment"
+# When provision through "Individual Enrollment", USE SYMMETRIC KEY DIRECTLY
 provisioning_host = "global.azure-devices-provisioning.net"
-id_scope = "{Scope Here}"
-registration_id = "{Device Name Here}"
-symmetric_key = "{Master Key Here}"
+id_scope = "{DPS Scope ID}"
+registration_id = "{Your Device ID}"
+symmetric_key = "{Your DPS Group Registration Key}"
+
 
 async def main():
-    
+    #'''
     # Connect using Device Provisioning Service (DPS)
     device_key = derive_device_key(registration_id,symmetric_key) #Convert from original symmetric key to device key for further enrollment
-    #print(device_key)
     provisioning_device_client = ProvisioningDeviceClient.create_from_symmetric_key(
         provisioning_host=provisioning_host,
         registration_id=registration_id,
@@ -45,124 +49,155 @@ async def main():
         symmetric_key=device_key
     )
     registration_result = await provisioning_device_client.register()
-    print("The Provision Status is :", registration_result.status)
-    print("The Provisioned ID is: ", registration_result.registration_state.device_id)
-    print("The Assigned IoT Hub is: ", registration_result.registration_state.assigned_hub)
-    print("The eTag is :", registration_result.registration_state.etag)
+
+    print("The status is :", registration_result.status)
+    print("The device id is: ", registration_result.registration_state.device_id)
+    print("The assigned IoT Hub is: ", registration_result.registration_state.assigned_hub)
+    print("The etag is :", registration_result.registration_state.etag)
 
     if registration_result.status == "assigned":
-        print("Provisioning Sucessfully, will send telemetry from the provisioned device now!")
+        print("Provisioning Sucessfully, will send telemetry from the provisioned device")
         device_client = IoTHubDeviceClient.create_from_symmetric_key(
             symmetric_key=device_key,
             hostname=registration_result.registration_state.assigned_hub,
             device_id=registration_result.registration_state.device_id,
         )
-
-        # Connect the client.
+    
+        # connect the client
         await device_client.connect()
 
-    # Update Device Information on Start
-    await device_client.patch_twin_reported_properties(device_property)
-    await device_client.patch_twin_reported_properties(location)
-
-
-    # define method handler
+    # define method handlers
     async def method_request_handler(method_request):
-        if method_request.name == "SetTelementryInternal":
-            global telemetry_interval
-            print(str(datetime.datetime.now()), "Recevied Request \"SetTelementryInternal\", Setting Interval to: " + str(telemetry_interval))
-            telemetry_interval = method_request.payload["Telemetry_Interval"]
+        if method_request.name == "Get_FW_info":
             # set response payload
-            payload = {"Time": str(datetime.datetime.now()), 
-                        "CallbackStatus": "200 OK", 
-                        "CallbackPayload": "The Interval is now: " + str(telemetry_interval)
-                        }
+            payload = {"result": True,
+                        "data": "The Firmware version now is " + str(fw_info)}
             status = 200  # set return status code
-            print(str(datetime.datetime.now()), "Processing Request \"SetTelementryInternal\" and Report, The Interval is now: " + str(telemetry_interval))
+            print(str(datetime.datetime.now()), "Processing Request \"Get_FW_info\" and Report, The Firmware version now is " + str(fw_info))
+            method_response = MethodResponse.create_from_method_request(
+                method_request, status, payload
+            )
+            # send response
+            await device_client.send_method_response(method_response)
+        
+        elif method_request.name =="SetTelementryInternal":
+            global telemetry_interval
+            telemetry_interval_tobe = method_request.payload["Telemetry_Interval"]
+            telemetry_interval = telemetry_interval_tobe
+            #print(send_data)
+            # set response payload
+            payload = {"result": telemetry_interval,
+                        "data": "The Send Data Status is " + str(telemetry_interval)}
+            status = 200  # set return status code
+            print(str(datetime.datetime.now()), "Processing Request \"SetTelemetryInterval\" and Report, The Telemetry Interval is now " + str(telemetry_interval))
             method_response = MethodResponse.create_from_method_request(
                 method_request, status, payload
             )
             # send response
             await device_client.send_method_response(method_response)
 
-            device_property_new_interval = {
-                    'Telemetry_Interval': telemetry_interval
-                }
-            await device_client.patch_twin_reported_properties(device_property_new_interval)
-
-        elif method_request.name == "SetTelemetrySwitch":
+        elif method_request.name =="SetTelemetrySwitch":
             global send_data
-            send_data = method_request.payload
-            print(str(datetime.datetime.now()), "Recevied Request \"SetTelemetrySwitch\", Setting Send_Data Switch to: " + str(send_data))            
-            payload = {"Time": str(datetime.datetime.now()),
-                        "CommandStatus": "200 OK",
-                        "CommandCallback": "The Send Data Status is: " + str(send_data)}
+            data = await device_client.get_twin()  # blocking call
+            #print(data)
+            send_data_now = send_data
+            send_data_tobe = method_request.payload
+            send_data = send_data_tobe
+            #print(send_data)
+            # set response payload
+            payload = {"result": send_data,
+                        "data": "The Send Data Status is " + str(send_data)}
             status = 200  # set return status code
-            print(str(datetime.datetime.now()), "Processing Request \"SetTelemetrySwitch\" and Report, The Send_Data Status is " + str(send_data))
+            print(str(datetime.datetime.now()), "Processing Request \"SetTelemetrySwitch\" and Report, The Send_Data Status is now " + str(send_data))
             method_response = MethodResponse.create_from_method_request(
                 method_request, status, payload
             )
             # send response
             await device_client.send_method_response(method_response)
 
-            device_property_new_switch = {
-                    'Telemetry_Switch': send_data
-                }
-            await device_client.patch_twin_reported_properties(device_property_new_switch)
-
+        elif method_request.name == "FW_Update":
+            fw_info_from_method = method_request.payload
+            print(str(datetime.datetime.now()), "Received Firmware Upgrade Request: Version " +
+                    str(fw_info_from_method)+ ", Initialiazing...")
+            time.sleep(2)
+            if fw_info >= fw_info_from_method:
+                payload = {"result": False,
+                        "data": ("The Firmware Version Now is " + str(fw_info) + ", Update Cancelled")}
+                status = 403  # set return status code
+                print(str(datetime.datetime.now()), "The Firmware Version is Latest, Firmware Upgrade Cancelled")
+                method_response = MethodResponse.create_from_method_request(
+                    method_request, status, payload
+                )
+                # send response
+                await device_client.send_method_response(method_response)
+            if fw_info < fw_info_from_method:
+                payload = {"result": True,
+                    "data": ("The Firmware Version Now is " + str(fw_info)+ ", Update Task Now Begin...")}
+                status = 200  # set return status code
+                method_response = MethodResponse.create_from_method_request(
+                    method_request, status, payload
+                )
+                # send response
+                await device_client.send_method_response(method_response)
+                print(str(datetime.datetime.now()), "Step 1: New Firmware Version " + str(fw_info_from_method),
+                        "is Set, Firmware Downloading...")
+                time.sleep(2)
+                print(str(datetime.datetime.now()), "Step 2: Downloading Success, Validation Firmware file...")
+                time.sleep(2)
+                print(str(datetime.datetime.now()), "Step 3: Firmware Validation Passed, Start Firmware Upgrading...")
+                time.sleep(2)
+                print(str(datetime.datetime.now()), "Step 4: Upgrading Sucessful, Rebooting Device...")
+                time.sleep(2)
+                print(str(datetime.datetime.now()), "Step 5: Device Successful Reconnected !!!")
+        
         else:
             # set response payload
             payload = {"result": False, "data": "Unrecognized Method"}
             status = 400  # set return status code
             print(str(datetime.datetime.now()), "Receiving Unknown Method: " + method_request.name)
+            print(method_request.payload)
             method_response = MethodResponse.create_from_method_request(
                 method_request, status, payload
             )
             # send response
             await device_client.send_method_response(method_response)
 
-    async def twin_patch_handler(data):
-        global fw_info
-        fw_info_new = data["Firmware_Info"]["value"]
-        print(str(datetime.datetime.now()), "Received Firmware Upgrade Request: Version " +
-                str(fw_info_new)+ ", Initialiazing...")
-        time.sleep(2)
-        if fw_info >= fw_info_new:
-            print(str(datetime.datetime.now()), "The Firmware Version is Latest, Firmware Upgrade Cancelled")
-
-        if fw_info < fw_info_new:
-            print(str(datetime.datetime.now()), "Step 1: New Firmware Version " + str(fw_info_new),
-                    "is Set, Firmware Downloading...")
-            time.sleep(2)
-            print(str(datetime.datetime.now()), "Step 2: Downloading Success, Validation Firmware file...")
-            time.sleep(2)
-            print(str(datetime.datetime.now()), "Step 3: Firmware Validation Passed, Start Firmware Upgrading...")
-            time.sleep(2)
-            print(str(datetime.datetime.now()), "Step 4: Upgrading Sucessful, Rebooting Device...")
-            time.sleep(2)
-            print(str(datetime.datetime.now()), "Step 5: Device Successful Reconnected !!!")
-            fw_info = fw_info_new
-
-            device_property_new_fw = {
-                'Firmware_Info': fw_info
-            }
-            await device_client.patch_twin_reported_properties(device_property_new_fw)
-
     # define behavior for receiving a message
-    async def message_receive_handler(device_client):
-        while True:
-            message = device_client.receive_message()  # blocking call
-            print(str(datetime.datetime.now()), "Received Message:")
-            print(message.data.decode())
-            if len(message.custom_properties) > 0:
-                print(str(datetime.datetime.now()), "With Custom Properties:")
-                print(message.custom_properties)
+    async def message_receive_handler(message):
+        print(str(datetime.datetime.now()), "Received Message:")
+        print(message.data.decode())
+        if len(message.custom_properties) > 0:
+            print("With Custom Properties:")
+            print(message.custom_properties)
 
+    # Twin Listener
+    async def twin_patch_handler(data):
+        global telemetry_interval, send_data
+        #data = patch  # blocking call
+        if "Telemetry_Interval" in data:
+            telemetry_interval = data["Telemetry_Interval"]
+            print(str(datetime.datetime.now()), "Telemetry Interval has set to", telemetry_interval)
+        if "Send_Data" in data:
+            send_data = data["Send_Data"]
+            if send_data == True:
+                print(str(datetime.datetime.now()), "Send Data has set to " + str(send_data) +
+                    ", Continue Sending Data...")
+            else:
+                print(str(datetime.datetime.now()), "Send Data has set to " + str(send_data) +
+                    ", Please update \"Send_Data\" to \"True\" in device-twin to restart!")
+        reported_properties = {
+            "Telemetry_Interval": telemetry_interval,
+            "Send_Data": send_data
+        }
+        await device_client.patch_twin_reported_properties(reported_properties)
+   
     # define send message to iot hub
     async def send_telemetry(device_client):
-        global telemetry_interval, send_data
+        global telemetry_interval, send_data, overload_value
         telemetry_data_raw = '{{"voltage": {voltage},"ampere": {ampere},"walt": {walt}}}'
         temp_telemetry_raw = '{{"Temperature": {temp},"Humidity": {humi}}}'
+        alert_data_raw = '{{"Alert": "{alert_text}"}}'
+
         while True:
             time.sleep(telemetry_interval)
             if send_data == True:
@@ -175,7 +210,7 @@ async def main():
                 print(str(datetime.datetime.now()),
                         "Sending Telemetry: ", device_telemetry_data)
 
-                
+                time.sleep(1)
                 Temperatureset = 20 + (random.random()*3)
                 Humidityset = 50 + (random.random()*6)
                 temp_telemetry_data_formatted = temp_telemetry_raw.format(
@@ -184,22 +219,33 @@ async def main():
                 print(str(datetime.datetime.now()),
                         "Sending Telemetry: ", temp_telemetry_data)
 
-                if waltset > 2.5:
-                    alert_info = "Capacity Over 90%"
-                    alert_msg_raw = '{{"Alert": "{alert}"}}'
-                    alert_msg = Message(alert_msg_raw.format(alert = alert_info), content_encoding= "utf-8", content_type="application/json")
-                    alert_msg.custom_properties["Alert"] = "Capacity Over 90%"
-                    alert_msg.message_id = uuid.uuid4()
-                    await device_client.send_message(alert_msg)
-                    print(str(datetime.datetime.now()), "Sending Telemetry: ", alert_msg)
+                if waltset >= overload_value:
+                    alert_info = "Overload"
+                    alertdata_formatted = alert_data_raw.format(alert_text = alert_info)
+                    alert_data = Message(alertdata_formatted, content_type="application/json", content_encoding="utf-8")
+                    temp_telemetry_data.custom_properties["Overload"] = "Yes"
+                    print(str(datetime.datetime.now()),
+                        "Sending Telemetry: ", alert_data)
+                    await device_client.send_message(alert_data)
 
                 await device_client.send_message(device_telemetry_data)
                 await device_client.send_message(temp_telemetry_data)
-
                 
+
+    async def property_patcher(device_client):
+        print("Updating Device Information on Start !!!")
+        reported_property1 = location
+        reported_property2 = device_property
+        await device_client.patch_twin_reported_properties(reported_property1)
+        await device_client.patch_twin_reported_properties(reported_property2)
+
     def send_telemetry_sync(device_client):
         loop = asyncio.new_event_loop()
         loop.run_until_complete(send_telemetry(device_client))
+
+    def twin_patch_sync(device_client):
+        loop = asyncio.new_event_loop()
+        loop.run_until_complete(property_patcher(device_client))
 
     def stdin_listener():
         while True:
@@ -210,13 +256,17 @@ async def main():
 
     # Set handlers to the client
     device_client.on_method_request_received = method_request_handler
-    device_client.on_twin_desired_properties_patch_received = twin_patch_handler
     device_client.on_message_received = message_receive_handler
+    device_client.on_twin_desired_properties_patch_received = twin_patch_handler
     
     send_telemetry_Thread = threading.Thread(target=send_telemetry_sync, args=(device_client,))
     send_telemetry_Thread.daemon = True
     send_telemetry_Thread.start()
 
+    send_telemetry_Thread = threading.Thread(target=twin_patch_sync, args=(device_client,))
+    send_telemetry_Thread.daemon = True
+    send_telemetry_Thread.start()
+    
     # Run the stdin listener in the event loop
     loop = asyncio.get_event_loop()
     user_finished = loop.run_in_executor(None, stdin_listener)
@@ -230,4 +280,10 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
+    # If using Python 3.6 or below, use the following code instead of asyncio.run(main()):
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.close()
 
